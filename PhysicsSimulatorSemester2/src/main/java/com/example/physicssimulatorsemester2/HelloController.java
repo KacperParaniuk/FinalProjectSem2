373a5a0c28ae93e436865982ecb268ef051704e2
KacperParaniuk <kacparaniuk@gmail.com>
2025-04-10 10:55:13 -0500

    4-10

--- PhysicsSimulatorSemester2/src/main/java/com/example/physicssimulatorsemester2/HelloController.java
+++ PhysicsSimulatorSemester2/src/main/java/com/example/physicssimulatorsemester2/HelloController.java
@@ -1,12 +1,15 @@
 package com.example.physicssimulatorsemester2;
 
 import javafx.animation.AnimationTimer;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
@@ -16,14 +19,17 @@ import static java.lang.Math.sin;
 public class HelloController {
     public Button resumeBtn;
     public Text angleLbl, velocityLbl;
+    public ToggleButton earthToggleButton, marsToggleButton, jupiterToggleButton;
+    public Text gravityLbl;
     @FXML
     private Canvas canvas;
+
 
     @FXML
     private Slider angleSlider;
     public Slider velocitySlider;
 
-    private double x,y;
+    private double x,y = 0;
     private double vx, vy;
     private double dt = 0.05;
     private double g = 9.8;
@@ -35,10 +41,15 @@ public class HelloController {
     public boolean stopped = false;
 
 
+
     private javafx.animation.AnimationTimer timer;
+
+    double scale = .2;
 
     public void initialize(){
         resumeBtn.setVisible(false);
+
+        gravityLbl.setText("Gravitational Acceleration: 9.81 m/s2");
 
         angleLbl.setText("Angle " + (int) angleSlider.getValue() +"°");
         velocityLbl.setText("Velocity " + (int) velocitySlider.getValue() + "m/s");
@@ -52,6 +63,9 @@ public class HelloController {
         velocitySlider.valueProperty().addListener((obs, oldVal, newVal) -> {
             velocityLbl.setText("Velocity " +newVal.intValue() + "m/s");
         });
+
+
+
     }
 
 
@@ -71,11 +85,16 @@ public class HelloController {
         vx = v0 * cos(angle);
         vy = -v0 * sin(angle); // negative so that it is upward at first
 
+        // ^ this is a unit of speed so we need to convert into pixels.
+
+
+        GraphicsContext gc = canvas.getGraphicsContext2D(); // this gives me my "drawing tool" for the canvas
+
 
         // canvas is from javafx that can be used as our drawing board where we will simulate!
 
-        GraphicsContext gc = canvas.getGraphicsContext2D(); // this gives me my "drawing tool" for the canvas
         gc.clearRect(0,0,canvas.getWidth(), canvas.getHeight()); // clearing canvas
+
 
         // creating the background
 
@@ -83,6 +102,7 @@ public class HelloController {
         gc.fillRect(0, canvas.getHeight()-10, canvas.getWidth(),10); // creates a ground
         gc.setFill(Color.SKYBLUE);
         gc.fillRect(0,0, canvas.getWidth(),380);
+
 
 
         timer = new AnimationTimer(){ // gives us access to the timer variable anywhere in our class.
@@ -112,6 +132,10 @@ public class HelloController {
         if(timer!= null){
             timer.stop();
         }
+        earthToggleButton.setVisible(true);
+        marsToggleButton.setVisible(true);
+        jupiterToggleButton.setVisible(true);
+        g = 9.81;
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.clearRect(0,0, canvas.getWidth(), canvas.getHeight());
         System.out.println("Reset");
@@ -156,7 +180,65 @@ public class HelloController {
         System.out.println(vy);
     }
 
-    public void addition(int x, int y){
-        int z = x + y;
+    public void toggleEarth(ActionEvent actionEvent) {
+        earthToggleButton.setVisible(true);
+        marsToggleButton.setVisible(false);
+        jupiterToggleButton.setVisible(false);
+        g = 9.81;
+        gravityLbl.setText("Gravitational Acceleration: 9.81 m/s2");
+    }
+
+
+    public void toggleMars(ActionEvent actionEvent) {
+        earthToggleButton.setVisible(false);
+        marsToggleButton.setVisible(true);
+        jupiterToggleButton.setVisible(false);
+        g = 3.73;
+        gravityLbl.setText("Gravitational Acceleration: 3.73 m/s2");
+
+    }
+
+    public void toggleJupiter(ActionEvent actionEvent) {
+        earthToggleButton.setVisible(false);
+        marsToggleButton.setVisible(false);
+        jupiterToggleButton.setVisible(true);
+        g = 24.79;
+        gravityLbl.setText("Gravitational Acceleration: 24.79 m/s2");
+
+    }
+
+
+
+    public void handleChangeAngle(MouseEvent mouseEvent) {
+        double angle = Math.toRadians(angleSlider.getValue());
+        System.out.println(angle);
+
+        vx = v0 * cos(angle);
+        vy = -v0 * sin(angle);
+
+
+    }
+
+    public void handleChangeVelocity(MouseEvent mouseEvent) {
+        GraphicsContext gc = canvas.getGraphicsContext2D(); // this gives me my "drawing tool" for the canvas
+
+        double angle = Math.toRadians(angleSlider.getValue());
+
+        vx = v0 * cos(angle);
+        vy = -v0 * sin(angle);
+
+        double initialX = 0;
+        double initialY = canvas.getHeight()-10;
+        double endX = x + vx * scale; // sets the magnitude
+        double endY = y + vy * scale;
+
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(2);
+        gc.strokeLine(initialX, initialY, endX, endY);
+
+
+
+
+
     }
 }
